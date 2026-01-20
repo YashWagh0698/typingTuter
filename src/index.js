a1acf86a8180a8f07f15c233e176b2ede86585a9
tecwithyash <yashwagh699@gmail.com>
2026-01-20 11:59:04 +0530

    css aded

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/variables.css";
+import "./styles/base.css";
+import "./styles/layout.css";
+import "./styles/components.css";
 
 // For accessibility: ensure React app mounts correctly
 const root = ReactDOM.createRoot(
