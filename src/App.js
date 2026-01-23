66bdc39425af4c31602d21f0d490f3ae9675cacc
tecwithyash <yashwagh699@gmail.com>
2026-01-23 11:41:27 +0530

    abcd

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
 import Levels from "./pages/Levels";
 import TutorialList from "./pages/TutorialList";
 import Task from "./pages/Task";
-import Result from "./pages/Result"; 
-import Welcome from "./pages/Welcome";
+import Result from "./pages/Result";
+
+import Header from "./components/Header";
+
 function App() {
   return (
     <BrowserRouter>
+      {/* 🔹 Global banner (visible on all pages) */}
+      <Header />
+
+      {/* 🔹 Main routing area */}
       <Routes>
         <Route path="/" element={<Welcome />} />
-<Route path="/practice" element={<Home />} />
-<Route path="/levels/:category" element={<Levels />} />
-<Route path="/tutorials/:category/:level" element={<TutorialList />} />
-<Route path="/task/:category/:level/:tutorial/:task" element={<Task />} />
-<Route path="/result/:category/:level/:tutorial" element={<Result />} />
+        <Route path="/practice" element={<Home />} />
+        <Route path="/levels/:category" element={<Levels />} />
+        <Route
+          path="/tutorials/:category/:level"
+          element={<TutorialList />}
+        />
+        <Route
+          path="/task/:category/:level/:tutorial/:task"
+          element={<Task />}
+        />
+        <Route
+          path="/result/:category/:level/:tutorial"
+          element={<Result />}
+        />
       </Routes>
     </BrowserRouter>
   );
