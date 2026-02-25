658a9f60b7c870cef9e1c0b40a5339d78afe9002
tecwithyash <yashwagh699@gmail.com>
2026-02-25 13:59:37 +0530

    Ensure Header import is included in Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import BackButton from "../components/BackButton";
+import Header from "../components/Header";
 
 export default function Home() {
   const navigate = useNavigate();
